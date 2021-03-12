5f3dd50b65c410c0e4504e633918b4935b6cba4a
Alberto Massidda <alberto.massidda@uniroma3.it>
2021-03-12 16:36:01 +0100

    added usage scripts

--- generate_perplexity.sh
+++ generate_perplexity.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+if [[ $# -lt 1 ]]; then
+echo "Usage: $0 <training log file>"
+exit 1
+fi
+
 grep ppl= -r $1 |awk '{print $2,$3,$NF}'|sed -r 's/[a-zA-Z]{1,3}=//g' > perplexity
--- generate_translations.sh
+++ generate_translations.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [[ $# -lt 1 ]]; then
+echo "Usage: $0 <model dir>"
+exit 1
+fi
+
 python3 -m sockeye.translate --models $1 --input test.3000.diff --output out.$1
