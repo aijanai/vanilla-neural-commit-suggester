09442b5ee3bb9fb5bbec6e01336c4d1c1e9e520a
Alberto Massidda <alberto.massidda@uniroma3.it>
2021-04-15 14:39:41 +0200

    added --force to evaluation script for Liu et al.

--- evaluate-liu.sh
+++ evaluate-liu.sh
@@ -5,5 +5,5 @@ echo "Usage: $0 <input file with translations>"
 exit 1
 fi
 
-sacrebleu --input $1 liu-dataset/cleaned.test.msg  --metrics bleu
+sacrebleu --input $1 liu-dataset/cleaned.test.msg  --metrics bleu --force
 rouge -f liu-dataset/cleaned.test.msg $1 --avg
