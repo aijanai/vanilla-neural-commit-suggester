dc0a44db6f015297b5c89909e851ece1a1e14089
Alberto Massidda <alberto.massidda@uniroma3.it>
2021-04-22 01:07:57 +0200

    retrained a failed run of the grid search

--- train-suggester-simple.sh
+++ train-suggester-simple.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 python3 -m sockeye.train \
-  --batch-size 64 \
+  --batch-size 48 \
   --batch-type sentence \
-  --device-ids -2 \
+  --device-ids 1 2 3 \
   --checkpoint-interval 3000 \
   --decode-and-evaluate 100 \
   --decoder ssru_transformer \
@@ -20,11 +20,11 @@ python3 -m sockeye.train \
   --cache-metric bleu \
   --optimizer adam \
   --num-embed 512:512 \
-  --num-layers 6:6 \
-  --transformer-attention-heads 8:8 \
+  --num-layers 8:8 \
+  --transformer-attention-heads 32:32 \
   --source train.26208.diff \
   --target train.26208.msg \
-  --output sockeye-commit-suggester \
+  --output sockeye-commit-suggester-grid-layer-8-head-32 \
   --validation-source test.3000.diff \
   --validation-target test.3000.msg
 #  --use-cpu \
